0ed206d6104e22be2092713d1af6a67f7efedb5d
Matija Pul <pulmatija@gmail.com>
2020-02-23 19:36:27 +0100

    Add unfinished implementation of python permute

---
+++ python/permutations/permutations.py
@@ -0,0 +1,23 @@
+def permute(inputList, k=0, outputList = None):
+    # outputList = []
+    # currentIndex = 0
+    # for number in inputList:
+    #     tempList = []
+    #     tempList.append(number)
+    #     for x : (len(inputList)-1)
+
+    #     outputList.append(tempList)
+    #     currentIndex += 1
+
+    # return outputList
+
+    # if k == len(inputList):
+    #     outputList.append(inputList)
+    #     return outputList
+    # else:
+    #     for i in xrange(k, len(inputList)):
+    #         inputList[k], inputList[i] = inputList[i] ,inputList[k]
+    #         outputList = permute(inputList, k+1, outputList)
+    #         inputList[k], inputList[i] = inputList[i], inputList[k]
+
+    return "Not finished yet"
